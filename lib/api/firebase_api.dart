005ce0f4db0af78208c4bf3c505035f6bf9e6ed9
adnan <adnankiswani123@gmail.com>
2024-05-02 09:43:17 +0300

    app update

---
+++ lib/api/firebase_api.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+import 'package:dhile/constant.dart';
+import 'package:dhile/models/notification.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  // final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  // LocalNoti.showBigTextNotification(title: message.notification?.title??'', body: message.notification?.body??'', fln: flutterLocalNotificationsPlugin);
+  // print('title:${message.notification?.title}');
+  // print('body:${message.notification?.body}');
+  // print('title:${message.data}');
+}
+
+
+class FirebaseApi {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+
+  Future<void> initNotifications() async {
+
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      sound: true,
+    );
+    final fCMToken = await _firebaseMessaging.getToken();
+    // print(settings.authorizationStatus);
+    // print('-------------------------------------------');
+    // print(fCMToken);
+    await _firebaseMessaging.subscribeToTopic('dhile');
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+      LocalNoti.showBigTextNotification(title:'${message.notification?.title}'.tr??"", body: '${message.notification?.body}'.tr??"",payload: message.data, fln: flutterLocalNotificationsPlugin);
+    });
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage event) {
+      NotificationModel notificationModel = notificationFromJson(jsonEncode(event.data));
+      Get.offAllNamed('/carNotificationDetails',arguments:[notificationModel.id]);
+    });
+
+
+//     await FirebaseMessaging.instance.getInitialMessage().then((value) {
+//
+//       // if(value!=null) {
+//       //   print('xxxxxxxxxxxxxxxxxx');
+//       //   print(value);
+//       //   print(value.data);
+//       //   NotificationModel notificationModel = notificationFromJson(jsonEncode(value.data));
+//       //   Get.offAllNamed('/carNotificationDetails', arguments: [notificationModel.id]);
+//       // }else{
+//       //   return null;
+//       // }
+// print('xxxxx');
+//     });
+    }
+}
+
+class LocalNoti {
+  static Future initialize(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var androidInitialize =
+        const AndroidInitializationSettings('mipmap/ic_launcher');
+    var iosInitialize = DarwinInitializationSettings();
+    var initializationSettings =
+        InitializationSettings(android: androidInitialize, iOS: iosInitialize);
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse details){
+
+        NotificationModel notificationModel = notificationFromJson(details.payload!);
+        Get.offAllNamed('/carNotificationDetails',arguments:[notificationModel.id]);
+
+
+      },
+    );
+
+  }
+
+  static Future showBigTextNotification(
+      {var id = 0,
+      required String title,
+      required String body,
+      var payload,
+      required FlutterLocalNotificationsPlugin fln}) async {
+   NotificationModel notificationModel = notificationFromJson(jsonEncode(payload));
+   final http.Response response = await http.get(Uri.parse('${Constant.domain}${notificationModel.image}'));
+   final http.Response brand = await http.get(Uri.parse('${Constant.domain}${notificationModel.brand}'));
+    AndroidNotificationDetails androidPlatformChannelSpecifics =
+         AndroidNotificationDetails(
+      'noti',
+      'channelName',
+      playSound: true,
+      importance: Importance.max,
+      priority: Priority.high,
+          styleInformation:   BigPictureStyleInformation(
+            ByteArrayAndroidBitmap.fromBase64String(base64Encode(response.bodyBytes)),
+            largeIcon: ByteArrayAndroidBitmap.fromBase64String(base64Encode(brand.bodyBytes)),
+          )
+    );
+    var not = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: DarwinNotificationDetails());
+    await fln.show(0, title, body, not,payload: jsonEncode(payload));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,76 @@
+import 'dart:convert';
+
+import 'package:dhile/api/firebase_api.dart';
 import 'package:dhile/constant.dart';
 import 'package:dhile/controller/HomeControllerBinding.dart';
 import 'package:dhile/lang/lang.dart';
+import 'package:dhile/models/notification.dart';
 import 'package:dhile/pages/home.dart';
 import 'package:dhile/routes.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:page_transition/page_transition.dart';
 import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive,overlays: []);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseApi().initNotifications();
+  LocalNoti.initialize(flutterLocalNotificationsPlugin);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Future<void> setupInteractedMessage() async {
+    // Get any messages which caused the application to open from
+    // a terminated state.
+    RemoteMessage? initialMessage =
+    await FirebaseMessaging.instance.getInitialMessage();
+
+    // If the message also contains a data property with a "type" of "chat",
+    // navigate to a chat screen
+    if (initialMessage != null) {
+      _handleMessage(initialMessage);
+    }
+
+    // Also handle any interaction when the app is in the background via a
+    // Stream listener
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  }
+
+  void _handleMessage(RemoteMessage message) {
+    print(message.data);
+    NotificationModel notificationModel = notificationFromJson(jsonEncode(message.data));
+    Get.offAllNamed('/carNotificationDetails', arguments: [notificationModel.id]);
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setupInteractedMessage();
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
