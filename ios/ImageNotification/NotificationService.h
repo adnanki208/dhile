e8932a8b5ac2276a76026db4da236dbe1e2ba3e4
imac <fadialkhlaf1998>
2024-05-12 11:08:33 +0400

    update ios

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by iMac on 06/05/2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
